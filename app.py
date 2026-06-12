798aabb33c507220479ce1118080e56ba89b4088
codewithniha <nihabatool10@gmail.com>
2026-06-12 16:43:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from transformers import pipeline
 import time, random, os
-#checking pipeline
+#checking pipeline whether it work or not
 app = Flask(__name__)
 
 classifier = pipeline(
