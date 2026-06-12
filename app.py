6c8b1b52588737d384caeeb5b1a9ab1df2563402
codewithniha <nihabatool10@gmail.com>
2026-06-12 14:08:23 +0500

    Add comment to check pipeline initialization

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from transformers import pipeline
 import time, random, os
-
+#checking pipeline
 app = Flask(__name__)
 
 classifier = pipeline(
