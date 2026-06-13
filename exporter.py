d4182dd1b9f920fa2af33812fd5083affa4667b5
codewithniha <nihabatool10@gmail.com>
2026-06-13 20:36:49 +0500

    Update exporter.py

--- exporter.py
+++ exporter.py
@@ -1,6 +1,9 @@
 import time
 import requests
 from prometheus_client import start_http_server, Gauge
+
+#checking jenins
+
 # This is the exact metric name the grading script checks
 CONFIDENCE_GAUGE = Gauge(
     'prediction_confidence_score',
